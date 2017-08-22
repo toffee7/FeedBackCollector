34d799ad9c50899b57fa2c588d86a3267d3ed398
Syed Tauseef Tanweer <Syed Tauseef Tanweer>
2017-08-23 04:09:01 +0530

    Routes refactored

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const passportConfig = require('./services/passport');
+require('./services/passport');
+const routes = require('./routes/authRoutes');
 const app = express();
 
+routes(app);
 
 //Dynamic port binding
 const PORT = process.env.PORT || 5000;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const passport = require('passport');
-const app = new express();
 
-//route setup for authentication
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile','email']
-}));
+module.exports = (app) => {
+    //route setup for authentication
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile','email']
+    }));
 
-//route setup for callback
-app.get('/auth/google/callback', passport.authenticate('google'));
+    //route setup for callback
+    app.get('/auth/google/callback', passport.authenticate('google'));
+};
+
+
