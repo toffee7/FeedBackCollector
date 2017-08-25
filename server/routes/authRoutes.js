0e5d677f6866d31c7f2f08d6266b17f63239a0b0
Tauseef <syed.tauseef.tanweer@citi.com>
2017-08-25 12:05:52 +0530

    Routes for github and linkedin added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,8 +7,22 @@ module.exports = (app) => {
         scope: ['profile','email']
     }));
 
+    app.get('/auth/github', passport.authenticate('github', {
+        scope: [ 'user:email']
+    }));
+
+    app.get('/auth/linkedin', passport.authenticate('linkedin', {
+        scope: ['r_basicprofile', 'r_emailaddress']
+    }))
+
     //route setup for callback
     app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get('/auth/github/callback', passport.authenticate('github'));
+
+    app.get('/auth/linkedin/callback', passport.authenticate('linkedin'));
+
+
 };
 
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const keys = require('../config/keys');
 
 //Google Strategy setup for Passport 
@@ -9,4 +11,34 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
     console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
 }));
+
+//LinkedIn Strategy setup for Passport
+passport.use(new LinkedInStrategy({
+    clientID: keys.linkedinClientID,
+    clientSecret: keys.linkedinClientSecret,
+    callbackURL: '/auth/linkedin/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
+}));
+
+//GitHub Strategy setup for Passport
+passport.use(new GitHubStrategy({
+    clientID: keys.githubClientID,
+    clientSecret: keys.githubClientSecret,
+    callbackURL: '/auth/github/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done);
+}));
+
+//Facebook Strategy setup for Passport
+
